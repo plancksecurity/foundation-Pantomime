a8fd57a6b51d4303f100b632959b4bd3d87bb328
Dirk Zimmermann <dirk@pep-project.org>
2016-04-05 15:57:19 +0200

    Basic xcode project with Pantomime

---
+++ PantomimeMailOSX/PantomimeMailOSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  PantomimeMailOSX
+//
+//  Created by Dirk Zimmermann on 05/04/16.
+//  Copyright © 2016 pEp Security S.A. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ PantomimeMailOSX/PantomimeMailOSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  PantomimeMailOSX
+//
+//  Created by Dirk Zimmermann on 05/04/16.
+//  Copyright © 2016 pEp Security S.A. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
