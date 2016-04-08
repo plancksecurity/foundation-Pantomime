b2e566ffaa5378e1eb51fceea51823ac56fdbada
Dirk Zimmermann <dirk@pep-project.org>
2016-04-08 15:37:12 +0200

    New iOS Xcode project

---
+++ PantomimeTests/PantomimeTests.swift
@@ -0,0 +1,36 @@
+//
+//  PantomimeTests.swift
+//  PantomimeTests
+//
+//  Created by Dirk Zimmermann on 08/04/16.
+//  Copyright © 2016 pEp Security S.A. All rights reserved.
+//
+
+import XCTest
+@testable import Pantomime
+
+class PantomimeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
