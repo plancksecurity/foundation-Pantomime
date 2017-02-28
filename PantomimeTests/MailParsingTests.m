f3d54f9774b45ec73310c985023aa3c298940fec
Dirk Zimmermann <dirk@pep-project.org>
2017-02-28 12:42:32 +0100

    IOS-457 fix test by using pantomime as it is

--- PantomimeTests/MailParsingTests.m
+++ PantomimeTests/MailParsingTests.m
@@ -19,14 +19,15 @@
 
 - (void)testParseFailureNotice
 {
-    NSData *emailData = [TestUtil loadDataWithFileName:@"MailParsingAttachments.txt"];
+    NSMutableData *emailData = [[TestUtil loadDataWithFileName:@"MailParsingAttachments.txt"]
+                                mutableCopy];
+    [emailData replaceCRLFWithLF]; // This is what pantomime does with everything it receives
     XCTAssertNotNil(emailData);
     CWIMAPMessage *msg = [[CWIMAPMessage alloc] initWithData:emailData];
     XCTAssertNotNil(msg);
     XCTAssertFalse([[msg content] isKindOfClass:[CWMIMEMultipart class]]);
     XCTAssertTrue([[msg content] isKindOfClass:[NSData class]]);
     XCTAssertNil(msg.contentType);
-    NSLog(@"|%@|", msg.subject);
     XCTAssertTrue([msg.subject isEqualToString:@"Failure Notice"]);
     XCTAssertEqualObjects(msg.subject, @"Failure Notice");
 }
