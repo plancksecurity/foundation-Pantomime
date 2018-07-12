2a6a3e98af0fffd5645755aa44cec9083a080510
buff <andreas@pep-project.org>
2018-07-12 16:01:05 +0200

    IOS-1172 blindly sets all headers that are not know to Pantomime.

--- pantomime-lib/Framework/Pantomime/CWMessage.m
+++ pantomime-lib/Framework/Pantomime/CWMessage.m
@@ -1153,14 +1153,15 @@ static CWRegEx *prefixSubjFwdHdrAndSuffixSubjFwdTrlRegex = nil;
         }
         [newMessageRawData appendCFormat:@"%s", LF];
     }
-    // We now set all X-* headers
+    // We now set all custom or non standard headers we are not aware of.
+    // E.g. headers prefixed with "X-".
     NSEnumerator *allHeaderKeyEnumerator = [_headers keyEnumerator];
-
     NSString *key;
     while ((key = [allHeaderKeyEnumerator nextObject]))  {
-//        if ([key hasPrefix:@"X-"] || ([key caseInsensitiveCompare:@"User-Agent"] == NSOrderedSame)) {
-        [newMessageRawData appendCFormat:@"%@: %@%s", key, [self headerValueForName: key], LF]; //IOS-1028: works for keyimport, but adds second date header that causes issues. Maybe adds other unwanted headers. //TODO: find good solution. Maybe provide Pantomime with custom headers.
-//        }
+        if ([CWMessageSupportedStandardHeaderFields containsObject:key]) {
+            continue;
+        }
+        [newMessageRawData appendCFormat:@"%@: %@%s", key, [self headerValueForName: key], LF];
     }
 
     // We add our message header/body separator
