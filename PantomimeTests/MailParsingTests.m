d656d13c9d48223f993851de49698a62b6c18e1a
buff <andreas@pep-project.org>
2018-09-07 19:18:36 +0200

    IOS-1225 adds repro test

--- PantomimeTests/MailParsingTests.m
+++ PantomimeTests/MailParsingTests.m
@@ -68,6 +68,14 @@
     XCTAssertEqualObjects(msg.from.personal, @"\"Test 001\"");
 }
 
+//IOS-1225
+- (void)testFromAddressIgnored
+{
+    CWIMAPMessage *msg = [self parseEmailFilePath:@"IOS-1225_from-address_not_ignored.txt"];
+    XCTAssertEqualObjects(msg.from.address, @"ebay@ebay.com");
+    XCTAssertEqualObjects(msg.from.personal, @"\"eBay\"");
+}
+
 /**
  IOS-1300
  */
