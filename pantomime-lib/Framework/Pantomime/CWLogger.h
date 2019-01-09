6cf01f95c008a84807dc86bded89d9b0e65abb87
Dirk Zimmermann <dz@pep.security>
2019-01-09 14:28:20 +0100

    IOS-1276 using @available

--- pantomime-lib/Framework/Pantomime/CWLogger.h
+++ pantomime-lib/Framework/Pantomime/CWLogger.h
@@ -8,15 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-// all those os/* for OS_LOG_TARGET_HAS_10_12_FEATURES
-#include <os/availability.h>
-#include <os/base.h>
-#include <os/object.h>
-#include <os/trace.h>
-
-#if OS_LOG_TARGET_HAS_10_12_FEATURES
 #import <os/log.h>
-#endif
 
 @protocol CWLogging <NSObject>
 
@@ -38,27 +30,27 @@
 
 @end
 
-#if OS_LOG_TARGET_HAS_10_12_FEATURES
 static os_log_t s_theLog;
 
 #define INFO(format, ...) \
-os_log_with_type(s_theLog, OS_LOG_TYPE_INFO, format, ##__VA_ARGS__);
+if (@available(macOS 10.12, iOS 10.0, watchOS 3.0, tvOS 10.0, *)) {\
+  NSLog(@"*** YES ***");\
+  os_log_with_type(s_theLog, OS_LOG_TYPE_INFO, format, ##__VA_ARGS__);\
+} else {\
+  NSLog(@"*** NO ***");\
+  NSLog([NSString stringWithCString:format encoding:NSUTF8StringEncoding], ##__VA_ARGS__);\
+}
 
 #define WARN(format, ...) \
-os_log_with_type(s_theLog, OS_LOG_TYPE_DEFAULT, format, ##__VA_ARGS__);
+if (@available(macOS 10.12, iOS 10.0, watchOS 3.0, tvOS 10.0, *)) {\
+  os_log_with_type(s_theLog, OS_LOG_TYPE_DEFAULT, format, ##__VA_ARGS__);\
+} else {\
+  NSLog([NSString stringWithCString:format encoding:NSUTF8StringEncoding], ##__VA_ARGS__);\
+}
 
 #define ERROR(format, ...) \
-os_log_with_type(s_theLog, OS_LOG_TYPE_ERROR, format, ##__VA_ARGS__);
-
-#else
-
-#define INFO(format, ...) \
-NSLog([NSString stringWithCString:format encoding:NSUTF8StringEncoding], ##__VA_ARGS__);
-
-#define WARN(format, ...) \
-NSLog([NSString stringWithCString:format encoding:NSUTF8StringEncoding], ##__VA_ARGS__);
-
-#define ERROR(format, ...) \
-NSLog([NSString stringWithCString:format encoding:NSUTF8StringEncoding], ##__VA_ARGS__);
-
-#endif
+if (@available(macOS 10.12, iOS 10.0, watchOS 3.0, tvOS 10.0, *)) {\
+  os_log_with_type(s_theLog, OS_LOG_TYPE_ERROR, format, ##__VA_ARGS__);\
+} else {\
+  NSLog([NSString stringWithCString:format encoding:NSUTF8StringEncoding], ##__VA_ARGS__);\
+}
