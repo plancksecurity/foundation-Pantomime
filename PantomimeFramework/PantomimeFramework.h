864ceeed80f5f13f3ada08e35be82229e6e99175
Dirk Zimmermann <dz@pep.security>
2019-02-15 11:33:18 +0100

    IOS-1480 Create Pantomime Framework.

---
+++ PantomimeFramework/PantomimeFramework.h
@@ -0,0 +1,19 @@
+//
+//  PantomimeFramework.h
+//  PantomimeFramework
+//
+//  Created by Dirk Zimmermann on 15.02.19.
+//  Copyright © 2019 pEp Security S.A. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for PantomimeFramework.
+FOUNDATION_EXPORT double PantomimeFrameworkVersionNumber;
+
+//! Project version string for PantomimeFramework.
+FOUNDATION_EXPORT const unsigned char PantomimeFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <PantomimeFramework/PublicHeader.h>
+
+
