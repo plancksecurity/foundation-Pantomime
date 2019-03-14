977cffabebb5df80f147b93d2778d978016c884d
Dirk Zimmermann <dz@pep.security>
2019-03-14 09:33:54 +0100

    IOS-1480 Move original files in the correct place.

--- PantomimeFramework/PantomimeFramework/PantomimeFramework.h
+++ PantomimeFramework/PantomimeFramework/PantomimeFramework.h
@@ -2,8 +2,8 @@
 //  PantomimeFramework.h
 //  PantomimeFramework
 //
-//  Created by Dirk Zimmermann on 13.03.19.
-//  Copyright © 2019 pEp Security. All rights reserved.
+//  Created by Dirk Zimmermann on 15.02.19.
+//  Copyright © 2019 pEp Security S.A. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
@@ -16,4 +16,23 @@ FOUNDATION_EXPORT const unsigned char PantomimeFrameworkVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <PantomimeFramework/PublicHeader.h>
 
-
+#import <PantomimeFramework/CWFolder.h>
+#import <PantomimeFramework/CWIMAPFolder.h>
+#import <PantomimeFramework/CWConstants.h>
+#import <PantomimeFramework/CWPart.h>
+#import <PantomimeFramework/CWService.h>
+#import <PantomimeFramework/CWStore.h>
+#import <PantomimeFramework/CWConnection.h>
+#import <PantomimeFramework/CWTransport.h>
+#import <PantomimeFramework/CWIMAPFolder.h>
+#import <PantomimeFramework/CWIMAPStore.h>
+#import <PantomimeFramework/CWSMTP.h>
+#import <PantomimeFramework/CWIMAPMessage.h>
+#import <PantomimeFramework/CWInternetAddress.h>
+#import <PantomimeFramework/CWFlags.h>
+#import <PantomimeFramework/CWCacheRecord.h>
+#import <PantomimeFramework/CWCacheManager.h>
+#import <PantomimeFramework/NSData+Extensions.h>
+#import <PantomimeFramework/CWIMAPCacheManager.h>
+#import <PantomimeFramework/CWMIMEMultipart.h>
+#import <PantomimeFramework/CWMIMEUtility.h>
