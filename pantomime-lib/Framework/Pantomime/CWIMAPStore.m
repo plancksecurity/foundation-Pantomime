b03dab334d2d4c52af2289e046187b7de14e8876
Dirk Zimmermann <dz@pep.security>
2019-09-27 15:54:22 +0200

    IOS-1748 Not 0-terminated, so this version.

--- pantomime-lib/Framework/Pantomime/CWIMAPStore.m
+++ pantomime-lib/Framework/Pantomime/CWIMAPStore.m
@@ -737,12 +737,24 @@ static inline int has_literal(char *buf, NSUInteger c)
             {
                 buf -= i; // go back to the beginning
 
-                char response[count];
-                int itemsAssigned = sscanf(buf, "%*s %s", response);
+                // Find the first space
+                char *response = strnstr(buf, " ", count);
 
-                if (itemsAssigned != 1) {
+                // No space -> no tagged response
+                if (response == NULL) {
                     [self _parseBAD];
                 } else {
+                    // pos is the index of the response, after the tag and the space
+                    long pos = response - buf + 1;
+
+                    // no space left for "OK" or "NO"?
+                    if (pos + 2 > (count - 1)) {
+                        [self _parseBAD];
+                    }
+
+                    // point to the response
+                    response++;
+
                     if (strncmp(response, "OK", 2) == 0) {
                         // From RFC3501:
                         //
