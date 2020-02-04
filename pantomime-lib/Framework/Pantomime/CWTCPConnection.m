80ad0aa3f1a65586a460f690e8ebad2815b6df68
Dirk Zimmermann <dz@pep.security>
2020-02-04 16:50:13 +0100

    IOS-1824 signalErrorAndClose

--- pantomime-lib/Framework/Pantomime/CWTCPConnection.m
+++ pantomime-lib/Framework/Pantomime/CWTCPConnection.m
@@ -239,6 +239,13 @@ NS_ASSUME_NONNULL_BEGIN
     }
 }
 
+- (void)signalErrorAndClose
+{
+    // We abuse ET_EDESC for error indication.
+    [self.forceDelegate receivedEvent:nil type:ET_EDESC extra:nil forMode:nil];
+    [self close];
+}
+
 @end
 
 @implementation CWTCPConnection (NSStreamDelegate)
@@ -278,9 +285,7 @@ NS_ASSUME_NONNULL_BEGIN
                 self.streamError = self.writeStream.streamError;
             }
 
-            // We abuse ET_EDESC for error indication.
-            [self.forceDelegate receivedEvent:nil type:ET_EDESC extra:nil forMode:nil];
-            [self close];
+            [self signalErrorAndClose];
 
             break;
         case NSStreamEventEndEncountered:
