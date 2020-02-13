a7457dbe7556842bb77b32778fe5d97123ffab99
Dirk Zimmermann <dz@pep.security>
2020-02-13 16:49:00 +0100

    IOS-1824 NSStream+Options

---
+++ pantomime-lib/Framework/Pantomime/NSStream+Options.h
@@ -0,0 +1,17 @@
+//
+//  NSStream+Options.h
+//  PantomimeFramework
+//
+//  Created by Dirk Zimmermann on 13.02.20.
+//  Copyright © 2020 pEp Security. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface NSStream (Options)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ pantomime-lib/Framework/Pantomime/NSStream+Options.m
@@ -0,0 +1,13 @@
+//
+//  NSStream+Options.m
+//  PantomimeFramework
+//
+//  Created by Dirk Zimmermann on 13.02.20.
+//  Copyright © 2020 pEp Security. All rights reserved.
+//
+
+#import "NSStream+Options.h"
+
+@implementation NSStream (Options)
+
+@end
