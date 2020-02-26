e5fa03db4a4ad478eec272460918d6398d648255
Dirk Zimmermann <dz@pep.security>
2020-02-26 16:59:43 +0100

    IOS-1824 Test connecting with client certificates

---
+++ PantomimeFramework/PantomimeFrameworkTests/Pantomime/ClientCertificateTest.m
@@ -0,0 +1,67 @@
+//
+//  ClientCertificateTest.m
+//  PantomimeFrameworkTests
+//
+//  Created by Dirk Zimmermann on 26.02.20.
+//  Copyright © 2020 pEp Security. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+#import <PantomimeFramework/PantomimeFramework.h>
+
+#import "CWCertificateLoader.h"
+
+static NSString *s_pfxBundleFilename = @"s_pfxBundleFilename";
+static NSString *s_pfxBundlePassword = @"s_pfxBundlePassword";
+static NSString *s_serverName = @"s_serverName";
+
+static NSTimeInterval s_timeout = 10;
+
+@interface ClientCertificateTest : XCTestCase
+
+@property (nonatomic) SecIdentityRef certificate;
+@property (nonatomic) XCTestExpectation *loggedIn;
+
+@end
+
+@implementation ClientCertificateTest
+
+- (void)setUp
+{
+    NSBundle *myBundle = [NSBundle bundleForClass:[self class]];
+    NSString *path2 = [myBundle pathForResource:s_pfxBundleFilename
+                                         ofType:nil];
+    NSData *p12data = [NSData dataWithContentsOfFile:path2];
+    self.certificate = [CWCertificateLoader secIdentityData:p12data
+                                                   password:s_pfxBundlePassword];
+    XCTAssertNotNil((__bridge id) self.certificate);
+}
+
+- (void)tearDown
+{
+}
+
+- (void)testIMAP
+{
+    self.loggedIn = [self expectationWithDescription:@"loggedIn"];
+
+    CWIMAPStore *imap = [[CWIMAPStore alloc] initWithName:s_serverName
+                                                     port:993
+                                                transport:ConnectionTransportTLS
+                                        clientCertificate:self.certificate];
+    imap.delegate = self;
+    [imap connectInBackgroundAndNotify];
+    [self waitForExpectations:@[self.loggedIn] timeout:s_timeout];
+}
+
+@end
+
+@implementation ClientCertificateTest (CWServiceClient)
+
+- (void) serviceInitialized: (NSNotification * _Nullable) theNotification
+{
+    NSLog(@"serviceInitialized");
+}
+
+@end
