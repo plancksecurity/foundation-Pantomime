eef0b3b9debf5af2e20f599e45170c3f46a459d9
Dirk Zimmermann <dz@pep.security>
2021-04-29 11:48:17 +0200

    IOS-2794 Docs

--- PantomimeFramework/PantomimeFramework/NSData+Extensions.h
+++ PantomimeFramework/PantomimeFramework/NSData+Extensions.h
@@ -257,9 +257,8 @@
 */
 - (NSArray *) componentsSeparatedByCString: (const char *) theCString;
 
-/// Like `componentsSeparatedByCString`, but calls the given block with each result and the current count.
-/// The block will be invoked for every component, together with its count and a boolean denoting if it's the last element.
-/// @note Uses `subdataUncopiedWithRange` to produce the `NSData` parts.
+/// Like `componentsSeparatedByCString`, but calls the given block with each component, the current count, .
+/// and a boolean denoting if it's the last element.
 - (void)componentsSeparatedByCString:(const char *)theCString
                                block:(void (^)(NSData *aLine, NSUInteger count, BOOL isLast))block;
 
